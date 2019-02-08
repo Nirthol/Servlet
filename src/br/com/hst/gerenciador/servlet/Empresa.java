e1a328b399ca20a8a2cfc80b97b1a8f429d36099
Guilherme Calvi Ferreira <guilherme_ferreira@hst.br>
2019-02-08 10:26:46 -0200

    abertura

---
+++ src/br/com/hst/gerenciador/servlet/Empresa.java
@@ -0,0 +1,20 @@
+package br.com.hst.gerenciador.servlet;
+
+public class Empresa {
+
+	private Integer id;
+	private String nome;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ src/br/com/hst/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,39 @@
+package br.com.hst.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+
+    @Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+    	System.out.println("Cadastrando nova empresa.");
+    	
+    	String nomeEmpresa = req.getParameter("nome");
+    	Empresa empresa = new Empresa();
+    	empresa.setNome(nomeEmpresa);
+    	
+    	Banco banco = new Banco();
+    	banco.adiciona(empresa);
+    	
+    	//Chamar o JSP
+    	RequestDispatcher rd = req.getRequestDispatcher("/novaEmpresaCriada.jsp");
+    	req.setAttribute("empresa", empresa.getNome());
+    	rd.forward(req, resp);
+    
+    }
+
+}
